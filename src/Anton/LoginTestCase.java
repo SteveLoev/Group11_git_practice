249864be5f222e52bd9b8776c620ba9c984dc036
Anton Smirnov <antonsmirnov4u@yahoo.com>
2022-01-24 16:20:18 -0500

    anton- initial commit

---
+++ src/Anton/LoginTestCase.java
@@ -0,0 +1,9 @@
+package Anton;
+
+public class LoginTestCase {
+
+    public static void main(String[] args) {
+
+        System.out.println("test");
+    }
+}
--- src/test/TC_001.java
+++ src/test/TC_001.java
@@ -1,4 +1,6 @@
 package test;
 
 public class TC_001 {
+
+
 }
